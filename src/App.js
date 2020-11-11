694aaec71403099fd55d8686955aa25b13d41eb8
tfsn <tfsn86@gmail.com>
2020-11-11 20:06:32 +0100

    API call setup and first layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_key = 'cd7f0b57eaef7dd77db85c3ff318ae08';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			location: '',
+			weatherData: {},
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	componentDidMount() {
+		fetch(
+			`http://api.openweathermap.org/data/2.5/weather?q=Copenhagen&units=metric&APPID=${API_key}`
+		).then((response) => {
+			response.json().then((data) => {
+				this.setState({
+					weatherData: data,
+				});
+			});
+		});
+	}
+
+	handleChange(event) {
+		const { name, value } = event.target;
+		this.setState({ [name]: value });
+	}
+
+	render() {
+		if (!this.state.weatherData.weather) {
+			return <span>Loading...</span>;
+		}
+
+		const temp = parseInt(this.state.weatherData.main.temp);
+
+		return (
+			<div>
+				<form
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						marginTop: 150,
+						marginBottom: 50,
+					}}
+				>
+					<label>
+						Location:
+						<input
+							style={{ marginLeft: 10 }}
+							type="text"
+							value={this.state.location}
+							name="location"
+							onChange={this.handleChange}
+						/>
+					</label>
+					<button style={{ marginLeft: 10 }}>Submit</button>
+				</form>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<h1 style={{ margin: 5 }}>{temp} °C</h1>
+					<h3 style={{ margin: 10 }}>
+						{this.state.weatherData.name}, {this.state.weatherData.sys.country}
+					</h3>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
